d9183fdb08c48340653a998f11c5735b987f5ef3
Sebastian Bonilla Cruz <sebastian.bonillac@udea.edu.co>
2025-08-03 18:22:16 -0500

    Initializing the project

---
+++ ArkaProject/src/main/java/org/sebasbocruz/arkaproject/ArkaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.sebasbocruz.arkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkaProjectApplication.class, args);
+    }
+
+}
---
+++ ArkaProject/src/test/java/org/sebasbocruz/arkaproject/ArkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebasbocruz.arkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
