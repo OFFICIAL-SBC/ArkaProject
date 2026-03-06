1f91ca8e7ef5d4c3a5d92e07f38c0ba1a496e12a
Sebastian Bonilla Cruz <sebastian.bonillac@udea.edu.co>
2026-03-05 21:13:49 -0500

    Creating the pipeline for creating a new Order

--- ms_orders/src/main/java/org/sebasbocruz/ms_orders/infrastructure/adapters/Mappers/OrderMapper.java
+++ ms_orders/src/main/java/org/sebasbocruz/ms_orders/infrastructure/adapters/Mappers/OrderMapper.java
@@ -1,15 +1,29 @@
 package org.sebasbocruz.ms_orders.infrastructure.adapters.Mappers;
 
+import org.sebasbocruz.ms_orders.domain.commons.states.OrderState;
 import org.sebasbocruz.ms_orders.domain.context.orders.Aggregate.Order;
 import org.sebasbocruz.ms_orders.infrastructure.adapters.persistence.schemas.Order.CartEntity;
+import org.sebasbocruz.ms_orders.infrastructure.adapters.persistence.schemas.Order.OrderEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderMapper {
 
-
-    public Order fromInfrastructureToDomain(CartEntity cartEntity){
-        return new Order();
+    public Order fromInfrastructureToDomain(OrderEntity orderEntity){
+        return new Order(
+                orderEntity.getOrder_id(),
+                orderEntity.getClient_id(),
+                orderEntity.getUser_id(),
+                orderEntity.getWarehouse_id(),
+                orderEntity.getCurrency_id(),
+                OrderState.PENDING,
+                orderEntity.getTotal_price(),
+                orderEntity.getTaxes(),
+                0.256,
+                orderEntity.getDiscount(),
+                orderEntity.getCreatedAt(),
+                orderEntity.getUpdatedAt()
+        );
     }
 
 }
--- ms_orders/src/main/java/org/sebasbocruz/ms_orders/infrastructure/adapters/gateways/OrdersGatewayImpl.java
+++ ms_orders/src/main/java/org/sebasbocruz/ms_orders/infrastructure/adapters/gateways/OrdersGatewayImpl.java
@@ -6,12 +6,16 @@ import org.sebasbocruz.ms_orders.domain.commons.errors.EntityNotFoundException;
 import org.sebasbocruz.ms_orders.domain.context.orders.Aggregate.Order;
 import org.sebasbocruz.ms_orders.domain.context.orders.Gateways.OrdersGateway;
 import org.sebasbocruz.ms_orders.infrastructure.adapters.Mappers.OrderMapper;
+import org.sebasbocruz.ms_orders.infrastructure.adapters.persistence.schemas.Order.OrderEntity;
 import org.sebasbocruz.ms_orders.infrastructure.adapters.repositories.CartDetailRepository;
 import org.sebasbocruz.ms_orders.infrastructure.adapters.repositories.CartRepository;
+import org.sebasbocruz.ms_orders.infrastructure.adapters.repositories.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +24,7 @@ public class OrdersGatewayImpl implements OrdersGateway {
     private Logger logger = LoggerFactory.getLogger(OrdersGatewayImpl.class);
     private CartRepository cartRepository;
     private CartDetailRepository cartDetailRepository;
+    private OrderRepository orderRepository;
 
     private OrderMapper orderMapper;
 
@@ -29,7 +34,24 @@ public class OrdersGatewayImpl implements OrdersGateway {
         return cartRepository.findById(cartID)
                 .switchIfEmpty(Mono.error(new EntityNotFoundException("Cart with ID "+cartID+"was not found")))
                 .doOnNext(cartEntity ->logger.info("Cart found, creating order for cartId: {}", cartEntity.getCartId()))
-                .map(cartEntity -> {return orderMapper.fromInfrastructureToDomain(cartEntity);})
+                .flatMap(cartEntity -> {
+                        return orderRepository.save(
+                                OrderEntity.builder()
+                                .client_id(1)
+                                .user_id(1)
+                                .warehouse_id(2)
+                                .order_state_id(1)
+                                .currency_id(1)
+                                .total_price(856.36)
+                                .taxes(0.01)
+                                .discount(0)
+                                .createdAt(Instant.now())
+                                .updatedAt(Instant.now())
+                                        .build()
+                        );
+                    }
+                )
+                .map(orderEntity -> {return orderMapper.fromInfrastructureToDomain(orderEntity);})
                 .onErrorMap(
                         exception ->!(exception instanceof EntityNotFoundException),
                         exception -> new DataAccessException("Error trying to get CartByID", exception)
