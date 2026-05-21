19ce51cfb9d53cbc66c42a76315dd1b35e901967
Sebastian Bonilla Cruz <sebastian.bonillac@udea.edu.co>
2026-05-20 20:56:16 -0500

    Working on the Order Domain Aggregate

--- ms_orders/src/main/java/org/sebasbocruz/ms_orders/domain/context/orders/entity/Shipment.java
+++ ms_orders/src/main/java/org/sebasbocruz/ms_orders/domain/context/orders/entity/Shipment.java
@@ -1,12 +1,13 @@
 package org.sebasbocruz.ms_orders.domain.context.orders.entity;
 
 import java.time.Instant;
+import java.util.List;
 
 public class Shipment {
     private final Long shipmentId;
     private final Long warehouseId;
     private final WarehouseOrigin origin;
-    private final Distance distance;
+    private final double distance;
     private Instant estimatedArrival;
     private ShipmentStatus status;
     private final List<ShipmentItem> items;
@@ -15,13 +16,26 @@ public class Shipment {
             Long shipmentId,
             Long warehouseId,
             WarehouseOrigin origin,
-            Distance distance,
+            double distance,
             Instant estimatedArrival,
             List<ShipmentItem> items){
+
         if(items.isEmpty()) throw IllegalArgumentException("Empty Shipment");
         this.shipmentId = shipmentId;
         this.warehouseId = warehouseId;
-
     }
 
 }
+
+
+record ShipmentItem (
+     Long id,
+     Long productId,// snapshot
+     int quantity,
+     double unitPrice
+){}
+
+
+record WarehouseOrigin(String displayName, String city, String country) {}
+
+ enum ShipmentStatus { PREPARING, IN_TRANSIT, DELIVERED, CANCELLED }
